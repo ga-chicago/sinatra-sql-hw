ea71954f88c27705c976c3e11e0698abe8e2cc0b
jeffkrantz <jjkrantz@gmail.com>
2017-03-23 16:19:20 -0500

    first commit

---
+++ app.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+require 'json'
+require 'pg'
+
+conn = PGconn.open(:dbname => 'practice')
+
+get '/restaurants' do
+  res = conn.exec("SELECT * FROM restaurants;")
+  restaurants = []
+  res.each do |restaurant|
+    restaurants.push(restaurant)
+  end
+  restaurants.to_json
+end
+
+get '/restaurants/:id' do
+  id = params[:id]
+  res = conn.exec("SELECT id, name FROM restaurants WHERE id = #{id};")
+  res[0].to_json
+end
+
+post '/restaurants' do
+  data = eval(request.body.read)
+
+  name = data[:name]
+  cuisine = data[:cuisine]
+  price = data[:price]
+  health = data[:health]
+
+  conn.exec("INSERT INTO restaurants (name, cuisine, price, health) VALUES
+  ('#{name}','#{cuisine}', #{price}, '#{health}');")
+  "Success"
+end
