7bb614c7cff4a38b8403a72ddf3d4e417ba0c926
cSwormstedt <cswormstedt7@gmail.com>
2017-03-23 23:55:07 -0500

    add mountains gets and post

--- app.rb
+++ app.rb
@@ -41,3 +41,50 @@ delete '/restaurants' do
 
 end
 
+
+# Create a route that returns all of your food items
+# Create a route that returns a single food item using its ID
+# Create a route that creates a new food items
+# Create a route that updates an existing food item using its ID
+# Create a route that deletes a food item using its ID
+
+get '/mountains' do
+
+  res = conn.exec("SELECT * FROM mountains")
+  mountains = []
+  res.each do |mountain|
+    mountains.push(mountain)
+  end
+  mountains.to_json
+end
+
+get '/mountains/:id' do
+
+  id = params[:id]
+  res = conn.exec("SELECT id, name FROM mountains WHERE id = #{id};")
+  res[0].to_json
+end
+
+post '/mountains/:id' do
+
+  data = eval(request.body.read)
+
+  name = data[:name]
+  state = data[:state]
+  highest_peak = data[:highest_peak]
+  trails = data[:tails]
+
+  conn.exec("INSERT INFO mountains (name, state, highest_peak, trails) VALUES ('#{name}', '#{state}', '#{highest_peak}', '#{trail}');")
+
+  "Success"
+end
+
+
+
+
+
+
+
+
+
+
