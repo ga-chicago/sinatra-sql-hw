8659927782b07e6f193f642c6cd803cdac7d94db
Naomi Mine <naomi.mine@gmail.com>
2017-03-24 06:53:15 -0500

    finished

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'pg'
 
 conn = PGconn.open(:dbname => 'practice')
 
+
 get '/restaurants' do
   res = conn.exec("SELECT * FROM restaurants;")
   restaurants = []
@@ -30,4 +31,73 @@ post '/restaurants' do
   conn.exec("INSERT INTO restaurants (name, cuisine, price, health) VALUES
   ('#{name}','#{cuisine}', #{price}, '#{health}');")
   "Success"
+end
+
+patch '/restaurants/:id' do
+	data = eval(request.body.read)
+	id = params[:id]
+
+    name = data[:name]
+    cuisine = data[:cuisine]
+    price = data[:price]
+    health = data[:health]
+
+	res = conn.exec("UPDATE restaurants SET (name, cuisine, price, health) = ('#{name}','#{cuisine}', #{price}, '#{health}') WHERE id = #{id};")
+	"success"
+end
+
+
+delete '/restaurants/:id' do
+	id = params[:id]
+	res = conn.exec("DELETE FROM restaurants WHERE id = #{id};")
+	"Success"
+end
+
+
+get '/dishes' do
+	res = conn.exec('SELECT * FROM dishes;')
+
+	dishes = []
+	res.each do |dish|
+		dishes.push(dish)
+	end
+	dishes.to_json
+end
+
+get '/dishes/:id' do
+  id = params[:id]
+  res = conn.exec("SELECT id, type FROM dishes WHERE id = #{id};")
+  res[0].to_json
+end
+
+post '/dishes' do
+	data = eval(request.body.read)
+
+	type = data[:type]
+	color = data[:color]
+	price = data[:price]
+	breakability = data[:breakability]
+
+	conn.exec("INSERT INTO dishes (type, color, breakability, price) VALUES
+	('#{type}','#{color}', #{breakability}, '#{price}');")
+	"Success"
+end
+
+patch '/dishes/:id' do
+	data = eval(request.body.read)
+	id = params[:id]
+
+	type = data[:type]
+	color = data[:color]
+	price = data[:price]
+	breakability = data[:breakability]
+
+	res = conn.exec("UPDATE dishes SET (type, color, breakability, price) = ('#{type}','#{color}', #{breakability}, '#{price}') WHERE id = #{id};")
+	"success"
+end
+
+delete '/dishes/:id' do
+	id = params[:id]
+	res = conn.exec("DELETE FROM dishes WHERE id = #{id};")
+	"Success"
 end
